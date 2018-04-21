3de1fb68d12149f0af416e9e1e4c3cc0f97f47a2
Széplaki Bence <szeplaki.bence@outlook.com>
2018-04-21 13:27:53 +0200

    added validations for email, and rentel name

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,3 +1,7 @@
 class Booking < ApplicationRecord
     belongs_to :rental
+    before_save :downcase_email
+    VALID_EMAIL = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+    validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
 end
